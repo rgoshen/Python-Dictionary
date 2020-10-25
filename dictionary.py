83265ad29058f274368ef42de889691ce13ed3e4
rgoshen <rick.goshen@gmail.com>
2020-10-25 13:34:54 -0700

    added docstrings

--- dictionary.py
+++ dictionary.py
@@ -1,9 +1,24 @@
+"""
+This program allows the user to enter in a word, proper 
+noun or acronym to search in data file and returns definition(s).
+
+Created by: R. Goshen
+Date: 10-25-2020
+
+I followed a tutorial from a course named "The Python Mega Course: 
+Build 10 Real World Applications" on stackskills.    
+"""
+
 import json
 from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
 def look_up(word):
+    """
+    This function looks up a word, proper noun, or acronym in data file
+    returns definition(s).
+    """
     word = word.lower()
     if word in data:
         return data[word]
